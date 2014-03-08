f13dd4fbe52cbefba7c27e6b97a966f452d48a9f
Daniel Garcia <daniel.garcia.fornells@gmail.com>
2014-03-08 11:03:23 +0100

    Add index.php file (widget entry point)

---
+++ src/index.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+
+<!--[if lt IE 7 ]> <html class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html class="">
+<!--<![endif]-->
+
+<head>
+    <meta charset="utf-8" />
+    <title>HKG Widget</title>
+    <meta id="projectViewport" name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--[if lt IE 9 ]>
+    <!-- build:script oldExplorer --><!-- /build -->
+    <![endif]-->
+
+    <link type="text/css" rel="stylesheet" href="assets/css/styles.css" />
+
+</head>
+
+<body>
+
+<!-- build:section main --><!-- /build -->
+
+<!-- build:script libraries --><!-- /build -->
+
+<!-- build:script templates --><!-- /build -->
+
+<!-- build:script models --><!-- /build -->
+
+<!-- build:script collections --><!-- /build -->
+
+<!-- build:script views --><!-- /build -->
+
+<!-- build:script dist --><!-- /build -->
+
+<!-- build:script inline mainApp -->
+<script type="text/javascript">
+    mainApp();
+</script>
+<!-- /build -->
+
+</body>
+
+</html>

d1cdaac1db3865c432a3ad9865da94d5f0a6e7c0
Daniel Garcia <daniel.garcia.fornells@gmail.com>
2014-03-08 11:03:58 +0100

    Add automation Grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,80 @@
+module.exports = function(grunt) {
+
+    // Init some params to run our tasks
+    grunt.params = {
+        js: {
+            // Define the Bower components directory (where bower stores the libraries)
+            folder: 'bower_components/',
+            // Define the path to original third party library files (without minification)
+            originalFiles: [
+                '<%= grunt.params.js.folder %>jquery/jquery.js',
+                '<%= grunt.params.js.folder %>underscore/underscore.js',
+                '<%= grunt.params.js.folder %>backbone/backbone.js',
+                '<%= grunt.params.js.folder %>underscore.string/lib/underscore.string.js',
+                '<%= grunt.params.js.folder %>backbone-deep-model/distribution/deep-model.js',
+                '<%= grunt.params.js.folder %>polyglot/build/polyglot.js'
+            ],
+            // Define the dev destination javascripts directory (to store third party library files)
+            parsedFolder: 'assets/js/',
+            // Define the path to copied third party library files
+            // Preserve this order to concatenate, referrers, ...
+            parsedFiles: [
+                '<%= grunt.params.js.parsedFolder %>jquery.js',
+                '<%= grunt.params.js.parsedFolder %>underscore.js',
+                '<%= grunt.params.js.parsedFolder %>backbone.js',
+                '<%= grunt.params.js.parsedFolder %>underscore.string.js',
+                '<%= grunt.params.js.parsedFolder %>deep-model.js',
+                '<%= grunt.params.js.parsedFolder %>polyglot.js'
+            ]
+        },
+        // Here we refer to those libraries used to enable IE<=8 modern browser features
+        oldExplorerJs: {
+            // Define the Bower components directory (where bower stores the libraries)
+            folder: 'bower_components/',
+            // Define the path to original third party library files (without minification)
+            originalFiles: [
+                '<%= grunt.params.oldExplorerJs.folder %>modernizr/modernizr.js',
+                '<%= grunt.params.oldExplorerJs.folder %>es5-shim/es5-shim.js',
+                '<%= grunt.params.oldExplorerJs.folder %>es5-shim/es5-sham.js',
+                '<%= grunt.params.oldExplorerJs.folder %>respond/src/respond.js'
+            ],
+            // Define the dev destination javascripts directory (to store third party library files)
+            parsedFolder: 'assets/js/old-explorer/',
+            // Define the path to copied third party library files
+            // Preserve this order to concatenate, referrers, ...
+            parsedFiles: [
+                '<%= grunt.params.oldExplorerJs.parsedFolder %>modernizr.js',
+                '<%= grunt.params.oldExplorerJs.parsedFolder %>es5-shim.js',
+                '<%= grunt.params.oldExplorerJs.parsedFolder %>es5-sham.js',
+                '<%= grunt.params.oldExplorerJs.parsedFolder %>respond.js'
+            ]
+        }
+    };
+
+    // Project configuration.
+    grunt.initConfig({
+        hkgDev: {
+            params: {}
+        }
+    });
+
+    // Load in any and all tasks in the `tasks` folder
+    grunt.loadTasks('tasks');
+
+    // Load any tasks in node_modules
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jst');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-file-creator');
+    grunt.loadNpmTasks('grunt-html-build');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-ftpscript');
+
+    // Init task.
+    grunt.registerTask('dev', ['hkgDev']);
+
+};
---
+++ tasks/grunt-hkg-dev.js
@@ -0,0 +1,94 @@
+module.exports = function(grunt) {
+
+    grunt.registerMultiTask('hkgDev', 'Description', function() {
+
+        // Tell grunt this task is asynchronous.
+        var done = this.async();
+
+        // Define the tasks list
+        var taskList = [];
+        // ------------------------
+        // Start Cleaning
+        var clean = {
+            init: [
+                'assets/css/*.css', // the compiled css
+                'assets/js/*', // the params js files
+                'src/templates/templates.js' // the compiled templates file
+            ]
+        };
+        grunt.config.set('clean', clean);
+        taskList.push('clean');
+        // ------------------------
+        // Start Copying
+        var copy = {
+            init: {
+                files: [
+                    {   // Javascript params
+                        expand: true,
+                        flatten: true,
+                        src: '{<%= grunt.params.js.originalFiles %>}',
+                        dest: 'assets/js/'
+                    },
+                    {   // Javascript oldExplorer params
+                        expand: true,
+                        flatten: true,
+                        src: '{<%= grunt.params.oldExplorerJs.originalFiles %>}',
+                        dest: 'assets/js/old-explorer/'
+                    }
+                ]
+            }
+        };
+        grunt.config.set('copy', copy);
+        taskList.push('copy');
+        // ------------------------
+        // JST Tasks
+        var jst = {
+            compile: {
+                // Compile all the templates into a single javascript file in the same folder
+                files: {
+                    "src/templates/templates.js": ["src/templates/**/*.html"]
+                }
+            }
+        };
+        grunt.config.set('jst', jst);
+        taskList.push('jst');
+        // ------------------------
+        // HTML Build tasks
+        var htmlbuild = {
+            init: {
+                src: 'src/index.php',
+                dest: '',
+                options: {
+                    beautify: true,
+                    /*styles: {
+                        main: 'assets/css/styles.css'
+                    },*/
+                    scripts: {
+                        oldExplorer: grunt.params.oldExplorerJs.parsedFiles,
+                        libraries: grunt.params.js.parsedFiles,
+                        templates: 'src/templates/templates.js',
+                        models: 'src/models/**/*.js',
+                        collections: 'src/collections/**/*.js',
+                        views: 'src/views/**/*.js',
+                        dist: {},
+                        mainApp: 'src/hkg.js'
+                    },
+                    sections: {
+                        main: 'src/templates/main.html'
+                    }
+                }
+            }
+        };
+        grunt.config.set('htmlbuild', htmlbuild);
+        taskList.push('htmlbuild');
+        // ------------------------
+        // ------------------------
+
+        // RUN ALL TASKS
+        grunt.task.run(taskList);
+
+        done();
+
+    });
+
+};
